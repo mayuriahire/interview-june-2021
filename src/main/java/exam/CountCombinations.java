dcdbaf944d33bb67dcf7576d17409bdb169b7e90
mayuriahire <86280687+mayuriahire@users.noreply.github.com>
2021-06-22 13:05:42 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -1,10 +1,28 @@
 package exam;
 
 public class CountCombinations {
-
-  public int getNumberOfWays(final int total, final int k) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
-  }
+	static int countWays(int n)
+	{
+		int DP[] = new int[n + 1];
+
+		
+		DP[0] = DP[1] = DP[2] = 1;
+		DP[3] = 2;
+
+		
+		for (int i = 4; i <= n; i++)
+			DP[i] = DP[i - 1] + DP[i - 3]
+					+ DP[i - 4];
+
+		return DP[n];
+	}
+
+
+	public static void main(String[] args)
+	{
+		int n = 10;
+		System.out.println(countWays(n));
+	}
 
 }
