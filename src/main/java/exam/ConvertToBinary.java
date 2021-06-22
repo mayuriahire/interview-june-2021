490835d763bb85a6e3b89ee2f0e17dd3b7fa6948
mayuriahire <86280687+mayuriahire@users.noreply.github.com>
2021-06-22 13:24:37 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -4,6 +4,22 @@ public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    int binary[] = new int[40];
+    int i = 0;
+    while(n>0)
+    {
+      binary[i++]=n%2;
+      n=n/2;
+    }
+    for(int j=i-1;j>=0;j--)
+    {
+      system.out.println(binary[j]);
+    } 
+    if(n<0)
+    {
+      throw new exception("not valid no");
+    }
+    
+    return "tobinary(35)";
   }
 }
