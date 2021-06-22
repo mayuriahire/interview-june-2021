2706860bed48dfe265d1827d6a28001fc1e45621
mayuriahire <86280687+mayuriahire@users.noreply.github.com>
2021-06-22 12:48:05 +0530

    java code for shift 1st element to last and last 1st store in same array

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -1,9 +1,15 @@
 package exam;
-
+import java.util.*;
 public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    int[] arr = [1,2,3,4,5]
+    system.out.println("origanal array"+arr);
+    int x=arr[0];
+    arr[0] = arr[arr.length-1];
+    arr[arr.length-1]=x;
+    system.out.println("new array"+arr);
 
   }
 
